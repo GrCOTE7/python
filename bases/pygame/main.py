602885980df96078bedad6dafe7d3d39f27b0220
GrCOTE7 <grcote7@gmail.com>
2024-07-22 11:39:17 +0200

    Feat: Player move

--- bases/pygame/main.py
+++ bases/pygame/main.py
@@ -1,37 +1,55 @@
 import pygame
+from player import Player
+
+
+class Game:
+    def __init__(self, screen):
+        self.screen = screen
+        self.running = True
+        self.clock = pygame.time.Clock()
+        self.player = Player(0, 0)
+
+    def handling_events(self):
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                self.running = False
+
+        keys = pygame.key.get_pressed()
+
+        if keys[pygame.K_LEFT]:
+            self.player.velocity[0] = -1
+        elif keys[pygame.K_RIGHT]:
+            self.player.velocity[0] = 1
+        else:
+            self.player.velocity[0] = 0
+        if keys[pygame.K_UP]:
+            self.player.velocity[1] = -1
+        elif keys[pygame.K_DOWN]:
+            self.player.velocity[1] = 1
+        else:
+            self.player.velocity[1] = 0
+
+    def update(self):
+        self.player.move()
+
+    def display(self):
+        self.screen.fill("white")
+        self.player.draw(self.screen)
+        pygame.display.flip()
+
+    def run(self):
+        while self.running:
+            self.handling_events()
+            self.update()
+            self.display()
+            self.clock.tick(60)
+
 
 pygame.init()
-screen = pygame.display.set_mode((800, 600))
-running = True
+screen = pygame.display.set_mode((1080, 720))
+game = Game(screen)
 
-image = pygame.image.load("ball.png").convert()
-
-clock = pygame.time.Clock()
-
-x = 0
-y = 0
-
-while running:
-
-    for event in pygame.event.get():
-
-        if event.type == pygame.QUIT:
-            running = False
-
-    pressed = pygame.key.get_pressed()
-
-    if pressed[pygame.K_LEFT]:
-        x -= 1
-    if pressed[pygame.K_RIGHT]:
-        x += 1
-    if pressed[pygame.K_UP]:
-        y -= 1
-    if pressed[pygame.K_DOWN]:
-        y += 1
-
-    screen.fill((0,0,0))
-    screen.blit(image, (x, y))
-    pygame.display.flip()
-    clock.tick(60)    
+game.run()
 
 pygame.quit()
