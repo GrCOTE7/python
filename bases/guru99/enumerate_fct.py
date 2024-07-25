243f48bcb588f1db7171c05796ad249bdc3c7e63
GrCOTE7 <grcote7@gmail.com>
2024-07-25 13:40:49 +0200

    Feat: enumerate()

--- bases/guru99/enumerate_fct.py
+++ bases/guru99/enumerate_fct.py
@@ -1,3 +1,9 @@
 n='lionel'
-print(*enumerate(n,7))
+print(*enumerate(n,7), '\n')
 
+my_dict = {"a": "PHP", "b":"JAVA", "c":"PYTHON", "d":"NODEJS"}
+print('Dict:', my_dict, '\n\nenumerate(Dict) :', *enumerate(my_dict), '\n')
+for i in enumerate(my_dict):
+  print("%s \nClé de l'énumérate : %s\nValue (Clé du tuple) : %s\n %s"
+        % (i, i[0], i[1], my_dict[i[1]]))
+  # print(i, my_dict[i])
