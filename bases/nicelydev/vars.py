c1671b71482d2a33103a2c1a8c812614c2fe75f3
GrCOTE7 <grcote7@gmail.com>
2025-01-26 14:07:38 +0100

    append, insert, remove

--- bases/nicelydev/vars.py
+++ bases/nicelydev/vars.py
@@ -1,12 +1,22 @@
 def msg():
+    for i in range(10):
+        print()
+
     ma_liste = ["un", "deux", "trois", "quatre", "cinq"]
 
     # Supprimer l'élément à l'index 1
     element = ma_liste.pop(1)
-    print(element)  # Affiche "deux"
-    print(ma_liste)  # Affiche ["un", "trois", "quatre", "cinq"]
+    print("element:", element)  # Affiche "deux"
+
+    ma_liste.append("six")
+    ma_liste.insert(0, "zero")
+    ma_liste.insert(2, "deux")
+    ma_liste.remove('zero')
+    print("ma_liste:", ma_liste)  # Affiche ["un", "trois", "quatre", "cinq"]
+
+    for i in range(7):
+        print()
 
 
 if __name__ == "__main__":
-
     msg()
