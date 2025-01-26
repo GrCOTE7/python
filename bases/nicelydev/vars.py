f3183ee5d42dafd44ae0fb3fa8eeb19d8aacc292
GrCOTE7 <grcote7@gmail.com>
2025-01-26 15:34:29 +0100

    dico del and clear()

--- bases/nicelydev/vars.py
+++ bases/nicelydev/vars.py
@@ -1,11 +1,40 @@
+import json
+from typing import Any, Optional
+
+
+def pjson(v: Optional[Any] = None, sortV: bool = False) -> None:
+    print(json.dumps(v, indent=2, sort_keys=sortV))
+    print()
+
+
 def msg():
     print()
 
-    a = {1, 3, 3, 4, 8}
-    
-    listA = list(a)
-    listA.sort() 
-    print(a, listA)
+    voiture = {"marque": "Toyota", "modele": "Yaris", "annee": 2019, "couleur": "rouge"}
+
+    pjson(voiture)
+
+    print(voiture["modele"])
+    print(voiture.get("modele"))
+
+    voiture["annee"] = 2025
+
+    pjson(voiture)
+
+    voiture["puissance"] = 100
+    voiture["places"] = "7".zfill(3)
+    pjson(voiture)
+
+    voiture.pop("places")
+    # del voiture["puissance"]
+    pjson(voiture)
+
+    # del voiture
+    # pjson(voiture) # → UnboundLocalError: cannot access local variable 'voiture' where it is not associated with a value
+    # pjson("voiture" in locals() and voiture or "voiture undefined")
+
+    # voiture.clear()
+    # pjson(voiture) # → {}
 
     print()
 
