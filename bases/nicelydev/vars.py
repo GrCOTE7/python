4770fdb022823e61a4bcfa87fc21abff43461fee
GrCOTE7 <grcote7@gmail.com>
2025-01-26 14:09:14 +0100

    count()

--- bases/nicelydev/vars.py
+++ bases/nicelydev/vars.py
@@ -2,16 +2,10 @@ def msg():
     for i in range(10):
         print()
 
-    ma_liste = ["un", "deux", "trois", "quatre", "cinq"]
+    ma_liste = ["un", "deux", "trois", "deux", "quatre", "cinq"]
 
-    # Supprimer l'élément à l'index 1
-    element = ma_liste.pop(1)
-    print("element:", element)  # Affiche "deux"
-
-    ma_liste.append("six")
-    ma_liste.insert(0, "zero")
-    ma_liste.insert(2, "deux")
-    ma_liste.remove('zero')
+    print(ma_liste.count('deux'))
+    
     print("ma_liste:", ma_liste)  # Affiche ["un", "trois", "quatre", "cinq"]
 
     for i in range(7):
