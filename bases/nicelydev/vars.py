d0601d2e3887f01a0f2264d5a6b0fe300dce87f8
GrCOTE7 <grcote7@gmail.com>
2025-01-26 14:28:01 +0100

    ensemble (set)

--- bases/nicelydev/vars.py
+++ bases/nicelydev/vars.py
@@ -1,15 +1,28 @@
 def msg():
     print()
 
-    nombres = 1, 2, 3, 1
+    nombres = 1, 2, 3, 1  # tuple
+    nombresSet = set(nombres)
+    print(nombresSet)
+    
+    informations = {"Mélanie", 10, 23, "Henrique"}
+    print(informations)
+    print(tuple(informations)[1])
+
+    nombres = {50, 50, 50, 10, 23, 12}
     print(nombres)
-    print(type(nombres)) # tuple immutable
-    print(len(nombres))
-    print(nombres.count(1))
-    print(nombres.index(2))
+    # Affiche : {50, 10, 12, 23}
+
+    prenoms = {'Pier', 'Pol', 'Jack'}
+    print(prenoms)
+    print('Pol' in prenoms)
+    
+    prenoms.add('Juda')
+    print(prenoms)
+    
+    prenoms.remove('Pol')
+    print(prenoms)
 
     print()
-
-
 if __name__ == "__main__":
     msg()
