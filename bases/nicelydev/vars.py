f95b41ab6c9e846e9aff21ccd5285d10ecb20abe
GrCOTE7 <grcote7@gmail.com>
2025-01-26 14:47:42 +0100

    list.sort()

--- bases/nicelydev/vars.py
+++ bases/nicelydev/vars.py
@@ -2,17 +2,10 @@ def msg():
     print()
 
     a = {1, 3, 3, 4, 8}
-    b = {2, 7, 3, 4, 5}
     
-    unionAB = a.union(b)
-
-    print("a: ", a, "- b:", b)
-    print("union: ", unionAB)
-    print("intersection: ", a.intersection(b))
-
-    print("a diff(b):", a.difference(b))
-    print("b diff(a):", b.difference(a))
-    print("a symmetric_diff(b):", a.symmetric_difference(b))
+    listA = list(a)
+    listA.sort() 
+    print(a, listA)
 
     print()
 
