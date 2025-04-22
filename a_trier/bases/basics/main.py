482a209c7d4651945d8a615e67be3443773ad471
GrCOTE7 <grcote7@gmail.com>
2025-04-22 17:54:48 +0200

    Camembert

--- a_trier/bases/basics/main.py
+++ a_trier/bases/basics/main.py
@@ -13,9 +13,55 @@ cls("Script tuto/")
 
 if __name__ == "__main__":
 
+    lg = "\n" + "-" * 55
     print("{0: ^55}".format("Tuple"))
     print()
-
-    print("-" * 55)
-    print("Ready.")
+    print(lg)
+    s = "Lionel"
+    print(s[2:4])
     print()
+
+    d = dict()
+    d["M-P", "R"] = 789
+    d["L", "C"] = 123
+
+    for f, l in d:
+        print(f"{f+'. ':<5} {l+'. ':<7}", ": ", d[f, l])
+
+    print("\n" + str(d))
+
+    n = d["L", "C"]
+    print(n)
+    print(": ".join(["L. C.", str(n)]))
+
+    print(d.keys())
+    print(d.values())
+    print(d.items())
+
+    print(lg)
+
+    print("Longueur=%s" % len(d))
+    print("Type=%s" % type(d))
+    d.update({("A", "K"): 456})
+    print(str(d) + "\n")
+
+    # print(d.items())
+
+    sorted_keys = sorted(d.keys())
+    for key in sorted_keys:
+        print(f"{str(key):<12} : ", d[key])
+    print(d.pop(("A", "K")))
+    print(lg)
+    sorted_keys = sorted(d.keys())
+    for key in sorted_keys:
+        print(f"{str(key):<12} : ", d[key])
+
+    print(lg)
+
+    a = [1, 2, 3]
+    # b = a
+    b = a[::]  # <=> b = a.copy()
+    b.append(4)
+
+    print(f"{a=}\n" + f"{b=}")
+    print(lg)
--- tuto/main.py
+++ tuto/main.py
@@ -1,21 +1,8 @@
-# Solution 1
 import sys
-
-# Solution 1
-# sys.path.append("c:\\laragon\\www\\PYTHON\\python\\tools")
-# print(sys.path)
-# quit()
-
-# Solution 2
 from pathlib import Path
 
 tools_path = Path(__file__).parent.parent / "tools"
-
-# Solution 3
-import os
-
-tools_path2 = os.path.join(os.path.dirname(os.path.dirname(__file__)), "tools")
-sys.path.append(str(tools_path2))
+sys.path.append(str(tools_path))
 
 from cls import cls
 
@@ -24,8 +11,6 @@ cls("Script tuto/")
 if __name__ == "__main__":
 
     print("{0: ^55}".format("x"))
-    print()
+    print("\n" + "-" * 55)
 
-    print(f"{tools_path = }")
-    print(f"{tools_path2 = }")
     print()
