88cefeab404c11dde754f83784300c17694801e6
GrCOTE7 <grcote7@gmail.com>
2025-07-14 19:42:15 +0200

    IHM avec tkinter

--- a_trier/bases/algomius/algorythmie/01_hanoi/main.py
+++ a_trier/bases/algomius/algorythmie/01_hanoi/main.py
@@ -1,4 +1,3 @@
-
 # https://www.youtube.com/watch?v=5KoTcO7LjhE&list=PLo53cbpzes8ZDG62Pn4U4plWpP8_EBFal&index=1
 # 2fix: Graphic version (https://drive.google.com/file/d/1uq7nVcOq_GDmYIIpdO5P58V1Oa2rN15f/view) that doesn't work
 def hanoi(nb_disques, depart, arrivee, intermediaire):
@@ -14,7 +13,7 @@ def hanoi(nb_disques, depart, arrivee, intermediaire):
         nb_deplacements_1 = hanoi(nb_disques - 1, depart, intermediaire, arrivee)
 
         # Affiche le déplacement effectué
-        # print("Déplacement de", depart, "vers", arrivee)
+        print("→ Déplacement de", depart, "vers", arrivee)
 
         # Appel récursif pour déplacer les disques restants de l'intermédiaire à l'arrivée
         nb_deplacements_2 = hanoi(nb_disques - 1, intermediaire, arrivee, depart)
@@ -22,8 +21,11 @@ def hanoi(nb_disques, depart, arrivee, intermediaire):
         # Retourne le nombre total de déplacements effectués
         return nb_deplacements_1 + nb_deplacements_2 + 1
 
-for i in range(1, 21):
-    print(
-        'Nombre de disques: ', i, ' → Nombre de déplacements:', hanoi(i, "Droite", "Gauche", "Milieu")
-    )
 
+for i in range(3, 3):
+    print(
+        "Nombre de disques: ",
+        i,
+        " → Nombre de déplacements:",
+        hanoi(i, "Droite", "Gauche", "Milieu"),
+    )
--- tuto/isc/main.py
+++ tuto/isc/main.py
@@ -1,17 +1,56 @@
 import flet as ft
 import gc7_positioned as gc7
+import tkinter
+
+
+def script() -> None:
+
+    arr = list(range(8))  # [0, 1, 2, 3, 4, 5, 6, 7]
+
+    arr2 = arr[:5:3]
+    print("arr", arr, "→", arr[:5])
+    print("arr2", arr2)
+
+
+def ihm():  # Interface Homme-Machine (IHM)
+
+    maFenetre = tkinter.Tk()
+    # maFenetre.title("IHM")
+
+    monLabel = tkinter.Label(maFenetre, text="Compteur : 0")
+
+    def onClick(event):
+        print("onClick sollicité")
+        global compteur
+        compteur = compteur + 1
+        # monLabel["text"] = f"Compteur : {compteur}"
+        monLabel.config(text=f"Compteur : {compteur}")
+        print(f"Compteur : {compteur}")
+
+    monBouton = tkinter.Button(maFenetre, text="Cliquer ICI !", width=50)
+    monBouton.pack()
+    monBouton.bind("<Button-1>", onClick)
+
+    # monLabel.pack(side=tkinter.TOP)
+    monLabel.pack()
+
+    maFenetre.geometry("400x200")
+    maFenetre.mainloop()
 
 
 def main(page: ft.Page):
-    arr = list(range(8))  # [0, 1, 2, 3, 4, 5, 6, 7]
-    
+
     gc7.position(page)
 
     txt = "Ready."
     t = ft.Text(txt, size=24, color=ft.Colors.ORANGE_500)
     page.add(t)
-    arr2 = arr[:5:3]
-    print('arr', arr, "→", arr[:5])
-    print('arr2', arr2)
 
-ft.app(target=main)
+
+if __name__ == "__main__":
+    script()
+
+    compteur = 0
+    ihm()
+    print("-" * 117)
+    # ft.app(target=main)
