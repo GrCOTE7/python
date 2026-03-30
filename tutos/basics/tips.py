378ba19223515c18c4d56ec961def77387bfdd8d
GrCOTE7 <grcote7@gmail.com>
2026-03-31 01:07:16 +0200

    Add tips

--- tutos/basics/tips.py
+++ tutos/basics/tips.py
@@ -2,13 +2,70 @@ import flet as ft
 from pymox_kit import cls, end
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 def main():
 
-    print("Tips en 2026")
+    print("Tips en 2026\n")
+
+    # var: int = 123
+    # print(f'{var = }') # permet d'afficher le nom de la variable et sa valeur')
+    # print(f'{var = !r}') # affiche la représentation de la variable (utile pour les chaînes de caractères)')
+    # print(f'{var = :.2f}') # affiche la variable avec 2 décimales (utile pour les nombres à virgule)')
+    # print(f'{var = : >7}') # aligne la variable à droite sur 10')
+    
+    # # try / except / else / finally
+    # a, b = 1, 0
+    # try:
+    #     print(f"{a} / {b} = {a / b:.2f}", end=" ")
+    # except ZeroDivisionError:
+    #     print("Erreur : Pas de division par zéro !")
+    # else:
+    #     print("(Division réussie)")
+    # finally:
+    #     print("Bloc finally exécuté")
+
+    names: list[str] = ["Alice", "Bob", "Charlie"]
+    # for i, name in enumerate(names, start=1):
+    print(*(f"{i}. {name}" for i, name in enumerate(names, start=1)), sep='\n', end='.\n\n')
+    print(*names, sep=', ', end='.\n')
+    
+
+
+
 
 
 if __name__ == "__main__":
-
     cls()
     main()
     end()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
