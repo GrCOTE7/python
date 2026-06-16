aa0c283af6a31c1f221d2cdf2a4ce7baedb3f1eb
GrCOTE7 <GrCOTE7@gmail.com>
2026-06-16 10:41:53 +0200

    ↑ bench comprehension

--- tutos/machinelearnia/benchmark_1_comprehension.py
+++ tutos/machinelearnia/benchmark_1_comprehension.py
@@ -1,28 +1,40 @@
+import flet as ft
+
+import sys
+from pathlib import Path
+
+sys.path.append(str(Path(__file__).resolve().parents[2]))
+
 from tools import *
 import time as top
 
 if __name__ == "__main__":
 
-    w = 55
+    def prevent(n, genre):
+        print(f"Je compte jusqu'à {nf(n,0)}... De façon {genre} :")
+
+    w = 45
     cls()
 
-    n = int(1e8)
-    prevent = f"Je compte jusqu'à {n}..."
-    print(prevent)
+    n = int(1e3) # 1e8
+    prevent(n, 'classique')
     s = top.time()
-    for i in range(n): # Ce i est global... <=> Recherche dans un dictionnaire
+    for i in range(n):  # Ce i est global... <=> Recherche dans un dictionnaire
         if i < 8:
             print(f"{i} ", sep=" ", end="")
 
     print(f"... {i}")
-    print(f"Boucle classique : {(top.time() - s):.2f}\"")
+    print(f'Boucle classique : {(top.time() - s):.2f}"')
+
+    sl()
     
-    sl(w)
-    print(prevent)
+    prevent(n, 'compréhension')
     s = top.time()
-    print(*(i for i in range(n) if i < 9), end="\b") # Ici, les var sont générées
+    print(*(i for i in range(n) if i < 9), end="\b")  # Ici, les var sont générées
     print(f"... {i}")
-    print(f"Boucle compréhension : {(top.time() - s):.2f}\"")
+    print(f'Boucle compréhension : {(top.time() - s):.2f}"')
 
     top.sleep(3)
     sl(w)
+
+    print('Fini.')
